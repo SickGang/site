f21096e5e8600b6abce9e8cc7c958f321985ae12
Maksim <max.ru6@mail.ru>
2021-03-12 17:28:41 +0400

    fix: add mobile version

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ $(function () {
   $(".company-accordion ul li").click(function () {
     $(this).toggleClass("active");
     $(this).find(".company-accordion_item-text").toggle(400);
-    // $(this).find("i").css("transform", "rotate(180deg)");
   });
+
+  $(".header-nav li a[href^='#'], .dropdown-menu a[href^='#']").click(
+    function () {
+      var fixed_offset = 150;
+      $("html,body")
+        .stop()
+        .animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    }
+  );
 });
