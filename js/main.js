4523a62737f42446fac7e672aebc13212ca02c2d
Maksim <max.ru6@mail.ru>
2021-03-13 20:05:49 +0400

    fix: main.js

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ $(function () {
     $(this).find(".company-accordion_item-text").toggle(400);
   });
 
-  $(".header-nav li a[href^='#'], .dropdown-menu a[href^='#']").click(
-    function () {
-      var fixed_offset = 150;
-      $("html,body")
-        .stop()
-        .animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
-    }
-  );
+  $(
+    ".header-nav li a[href^='#'], .dropdown-menu a[href^='#'], .header-img a[href^='#']"
+  ).click(function () {
+    var fixed_offset = 150;
+    $("html,body")
+      .stop()
+      .animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  });
 });
