3acea193974078051435d55b397a69834afcbdc2
Maksim <max.ru6@mail.ru>
2021-04-18 20:14:05 +0400

    .

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 $(function () {
-  $(".company-accordion ul li").click(function () {
-    $(this).toggleClass("active");
-    $(this).find(".company-accordion_item-text").toggle(400);
-  });
+    $('a.hero-video_link').YouTubePopUp({ autoplay: 0 }) // Disable autoplay
+    $('.company-accordion ul li').click(function () {
+        $(this).toggleClass('active')
+        $(this).find('.company-accordion_item-text').toggle(400)
+    })
 
-  $(
-    ".header-nav li a[href^='#'], .dropdown-menu a[href^='#'], .header-img a[href^='#']"
-  ).click(function () {
-    var fixed_offset = 150;
-    $("html,body")
-      .stop()
-      .animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
-  });
-});
+    $(
+        ".header-nav li a[href^='#'], .dropdown-menu a[href^='#'], .header-img a[href^='#'], .hero-button [href^='#']"
+    ).click(function () {
+        var fixed_offset = 150
+        $('html,body')
+            .stop()
+            .animate(
+                { scrollTop: $(this.hash).offset().top - fixed_offset },
+                1000
+            )
+    })
+})
